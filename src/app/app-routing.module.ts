ef44bed824b3952daac95af1fc49c0c17c2c3f1b
Rodrigo Santiago <rodrigosantiag@gmail.com>
2022-08-20 18:10:18 -0300

    Create page using reusable components Use switchmap instead of subscribe rel Add guards to prevent logged user to access /login and not logged user to access /animais

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AutenticacaoGuard } from './autenticacao/autenticacao.guard';
+import { LoginGuard } from './autenticacao/login.guard';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+    canLoad: [LoginGuard],
   },
   {
     path: 'animais',
     loadChildren: () =>
       import('./animais/animais.module').then((m) => m.AnimaisModule),
+    canLoad: [AutenticacaoGuard],
   },
 ];
 
