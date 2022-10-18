dba2aee560b5523ce2724ea76f193867b1b43f02
Rodrigo Santiago <rodrigosantiag@gmail.com>
2022-10-17 21:35:20 -0300

    Use ActivatedRoute Attach token to headers using Interceptor Add curtir() and excluir() functions

---
+++ src/app/animais/detalhe-animal/detalhe-animal.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Animal } from '../animais';
+import { AnimaisService } from '../animais.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-detalhe-animal',
+  templateUrl: './detalhe-animal.component.html',
+  styleUrls: ['./detalhe-animal.component.css'],
+})
+export class DetalheAnimalComponent implements OnInit {
+  animalID!: number;
+  animal$!: Observable<Animal>;
+  constructor(
+    private animaisService: AnimaisService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.animalID = this.activatedRoute.snapshot.params.animalId;
+    this.animal$ = this.animaisService.buscaPorID(this.animalID);
+  }
+
+  curtir() {
+    this.animaisService.curtir(this.animalID).subscribe((curtida) => {
+      if (curtida) {
+        this.animal$ = this.animaisService.buscaPorID(this.animalID);
+      }
+    });
+  }
+
+  excluir() {
+    this.animaisService.excluiAnimal(this.animalID).subscribe(
+      () => {
+        this.router.navigate(['/animais/']);
+      },
+      (error) => console.log(error)
+    );
+  }
+}
